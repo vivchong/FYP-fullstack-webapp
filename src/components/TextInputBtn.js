024299a556604b08c3f9ec9e7984ee69aa0ae97f
viv chong <vivienne.chong@outlook.com>
2022-08-26 01:57:11 +0800

    Connect to DB, push to DB only

--- src/components/TextInputBtn.js
+++ src/components/TextInputBtn.js
@@ -13,12 +13,42 @@ import {
   Container,
   ModalFooter,
   Button,
+  FormControl,
+  FormErrorMessage,
 } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Field, Formik, FormikProps } from 'formik';
+import PlainForm from '../pages/sig-dashboard/abouttab/PlainForm';
+import * as Yup from 'yup';
 
 // This is a button that looks like an Input field and opens a "Create Post" modal
 
 const TextInputBtn = props => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  // const createPostSchema = Yup.object().shape({
+  //   createPost: Yup.string().required("You can't post a discussion without words!"),
+  // });
+
+  const [post_content, setpost_content] = useState("");
+  const sig_id = 1;
+  const user_id = 1;
+  
+  const onSubmitForm = async e => {
+    e.preventDefault();
+    try {
+      const body = { sig_id, user_id, post_content };
+      const newPost = await fetch('http://localhost:5000/discussion_posts', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+      });
+      console.log(JSON.stringify(body));
+      window.location = '/';
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
   return (
     <>
@@ -46,26 +76,67 @@ const TextInputBtn = props => {
       >
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader></ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            <Container display="flex" gap={4} px={0}>
-              <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
-              <Textarea
-                variant="unstyled"
-                placeholder={props.children}
-                _placeholder={{ opacity: 0.36, color: 'black' }}
-                size="lg"
-                height="220px"
-              ></Textarea>
-            </Container>
-          </ModalBody>
-          <ModalFooter>
-            <Button onClick={onClose} mr={3}>
-              Cancel
-            </Button>
-            <Button colorScheme="teal">Post</Button>
-          </ModalFooter>
+          <Formik
+            initialValues={{
+              createPost: '',
+            }}
+            onSubmit={onSubmitForm} 
+          //   {(values) => {
+          //   alert(JSON.stringify(values, null, 2));
+          // }}
+            //validationSchema={createPostSchema} // validation is handled by Yup
+          >
+            {({ handleSubmit, values, errors, touched }) => (
+              <form onSubmit={onSubmitForm}>
+                <ModalHeader />
+                <ModalCloseButton />
+                <ModalBody pb={6}>
+                  <Container display="flex" gap={4} px={0}>
+                    <Avatar
+                      name="Dan Abrahmov"
+                      src="https://bit.ly/dan-abramov"
+                    />
+
+                    <FormControl
+                      isInvalid={!!errors.createPost && touched.createPost}
+                    >
+                      <Field
+                        as={Textarea}
+                        id="createPost"
+                        name="createPost"
+                        variant="unstyled"
+                        placeholder={props.children}
+                        _placeholder={{ opacity: 0.36, color: 'black' }}
+                        size="lg"
+                        height="220px"
+                        // validation is handled by Yup
+                        value={post_content}
+                        onChange={e => setpost_content(e.target.value)}
+                        validate={(value) => {
+                          let error;
+                          if (value.length < 0) {
+                            error =
+                              "You can't post a discussion without words!";
+                          }
+                          return error;
+                        }}
+                      />
+                      <FormErrorMessage>{errors.createPost}</FormErrorMessage>
+                    </FormControl>
+                   
+                  </Container>
+                </ModalBody>
+                <ModalFooter>
+                  <Button onClick={onClose} mr={3}>
+                    Cancel
+                  </Button>
+                  <Button colorScheme="teal" type="submit">
+                    Post
+                  </Button>
+                </ModalFooter>
+              </form>
+            )}
+          </Formik>
         </ModalContent>
       </Modal>
     </>
--- src/pages/sig-dashboard/abouttab/SIGTabsAbout.js
+++ src/pages/sig-dashboard/abouttab/SIGTabsAbout.js
@@ -16,6 +16,8 @@ import BaseCard from '../../../components/layout/cards/BaseCard';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 import MemberList from '../../../components/sig-dashboard/MemberList';
 
+
+
 // MEMBER LIST HANDLER has to be here.
 // This handler will find members of this SIG given the SIG ID from table:RoleInSIGs
 // and extract the UserID and ROLE where SIGID = this SIG's
@@ -108,110 +110,109 @@ const SIG_DETAILS = [
 
 const SIGTabsAbout = () => {
   return (
+    <Container /* 1-Column Flex Layout */
+      px={10}
+      pt={4}
+      maxW="856px"
+      width="100%"
+      display="flex"
+      flexDirection="column"
+      gap={8}
+    >
+      <BaseCard>
+        <VStack my={6} alignItems="flex-start" spacing={2}>
+          <Heading as="h3" size="md" fontWeight="medium">
+            About
+          </Heading>
 
-      <Container /* 1-Column Flex Layout */
-        px={10}
-        pt={4}
-        maxW="856px"
-        width="100%"
-        display="flex"
-        flexDirection="column"
-        gap={8}
-      >
-        <BaseCard>
-          <VStack my={6} alignItems="flex-start" spacing={2}>
-            <Heading as="h3" size="md" fontWeight="medium">
-              About
-            </Heading>
+          <Text as="p" /* preserves line breaks */>
+            {SIG_DETAILS[0].sigdescription
+              .split('\n')
+              .map(function (item, key) {
+                return (
+                  <span key={key}>
+                    {item}
+                    <br />
+                  </span>
+                );
+              })}
+          </Text>
 
-            <Text as="p" /* preserves line breaks */ >
-              {SIG_DETAILS[0].sigdescription
-                .split('\n')
-                .map(function (item, key) {
-                  return (
-                    <span key={key}>
-                      {item}
-                      <br />
-                    </span>
-                  );
-                })}
+        </VStack>
+      </BaseCard>
+
+      <BaseCard>
+        <VStack my={6} alignItems="flex-start" spacing={3}>
+          <Heading as="h3" size="md" fontWeight="medium">
+            Next meeting
+          </Heading>
+          <HStack spacing={3}>
+            <Text as="p" noOfLines={5} mr={4}>
+              {SIG_DETAILS[0].sigmeetingday}, 12 August
             </Text>
+            <Text as="span">{SIG_DETAILS[0].sigstarttime}</Text>
+            <Text as="span">—</Text>
+            <Text as="span">{SIG_DETAILS[0].sigendtime}</Text>
+          </HStack>
+          <Divider pt={1} colorScheme="gray" />
+          <VStack spacing={1} alignItems="flex-start">
+            <Text>
+              Meeting link:&nbsp;
+              <Link
+                href={SIG_DETAILS[0].sigmeetinglink}
+                color="teal.500"
+                isExternal
+              >
+                {SIG_DETAILS[0].sigmeetinglink}
+                <ExternalLinkIcon mx="2px" />
+              </Link>
+            </Text>
+            <Text>Password: {SIG_DETAILS[0].sigmeetingpassword}</Text>
           </VStack>
-        </BaseCard>
+        </VStack>
+      </BaseCard>
 
-        <BaseCard>
-          <VStack my={6} alignItems="flex-start" spacing={3}>
-            <Heading as="h3" size="md" fontWeight="medium">
-              Next meeting
-            </Heading>
-            <HStack spacing={3}>
-              <Text as="p" noOfLines={5} mr={4}>
-                {SIG_DETAILS[0].sigmeetingday}, 12 August
-              </Text>
-              <Text as="span">{SIG_DETAILS[0].sigstarttime}</Text>
-              <Text as="span">—</Text>
-              <Text as="span">{SIG_DETAILS[0].sigendtime}</Text>
-            </HStack>
-            <Divider pt={1} colorScheme="gray" />
-            <VStack spacing={1} alignItems="flex-start">
-              <Text>
-                Meeting link:&nbsp;
-                <Link
-                  href={SIG_DETAILS[0].sigmeetinglink}
-                  color="teal.500"
-                  isExternal
-                >
-                  {SIG_DETAILS[0].sigmeetinglink}
-                  <ExternalLinkIcon mx="2px" />
-                </Link>
-              </Text>
-              <Text>Password: {SIG_DETAILS[0].sigmeetingpassword}</Text>
-            </VStack>
-          </VStack>
-        </BaseCard>
+      <BaseCard>
+        <VStack my={6} alignItems="flex-start" spacing={2}>
+          <Heading as="h3" size="md" fontWeight="medium">
+            Updates
+          </Heading>
+          {isNaN(SIG_DETAILS[0].updates) ? (
+            <Text noOfLines={10} mr={4}>
+              There are currently no updates.
+            </Text>
+          ) : (
+            <Text noOfLines={10} mr={4}>
+              {SIG_DETAILS[0].updates}
+            </Text>
+          )}
+        </VStack>
+      </BaseCard>
 
-        <BaseCard>
-          <VStack my={6} alignItems="flex-start" spacing={2}>
-            <Heading as="h3" size="md" fontWeight="medium">
-              Updates
-            </Heading>
-            {isNaN(SIG_DETAILS[0].updates) ? (
-              <Text noOfLines={10} mr={4}>
-                There are currently no updates.
-              </Text>
-            ) : (
-              <Text noOfLines={10} mr={4}>
-                {SIG_DETAILS[0].updates}
-              </Text>
-            )}
-          </VStack>
-        </BaseCard>
+      <BaseCard>
+        <VStack my={6} alignItems="flex-start" spacing={4} width="100%">
+          <Flex width="100%" alignItems="center">
+            <Box>
+              <Heading
+                as="h3"
+                size="md"
+                fontWeight="medium"
+                alignContent="center"
+              >
+                {SIG_DETAILS[0].sigmembercount} Member
+                {SIG_DETAILS[0].sigmembercount > 1 && 's'}
+              </Heading>
+            </Box>
+            <Spacer />
+            <Button variant="ghost" size="sm">
+              Manage members
+            </Button>
+          </Flex>
 
-        <BaseCard>
-          <VStack my={6} alignItems="flex-start" spacing={4} width="100%">
-            <Flex width="100%" alignItems="center">
-              <Box>
-                <Heading
-                  as="h3"
-                  size="md"
-                  fontWeight="medium"
-                  alignContent="center"
-                >
-                  {SIG_DETAILS[0].sigmembercount} Member
-                  {SIG_DETAILS[0].sigmembercount > 1 && 's'}
-                </Heading>
-              </Box>
-              <Spacer />
-              <Button variant="ghost" size="sm">
-                Manage members
-              </Button>
-            </Flex>
-
-            <MemberList members={MEMBERS} />
-          </VStack>
-        </BaseCard>
-      </Container>
-
+          <MemberList members={MEMBERS} />
+        </VStack>
+      </BaseCard>
+    </Container>
   );
 };
 
